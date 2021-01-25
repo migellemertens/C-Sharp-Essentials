02d691bc0806c768c21c9c1ed37fd33dc7827570
Migelle Mertens <migelle.mertens@gmail.com>
2021-01-25 14:18:16 +0100

    oef

---
+++ Toepassing 20 - Vermenigvuldigingstafels/Toeppasing 20 - Vermenigvuldigingstafels/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toeppasing_20___Vermenigvuldigingstafels
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBerekenen_Click(object sender, RoutedEventArgs e)
+        {
+            int getal = Convert.ToInt32(TxtGetal.Text);
+
+            for(int i = 1; i <= getal; i++)
+            {
+                for(int j =1; j <= 10; j++)
+                {
+                    TxtResultaat.Text += $"{i} * {j} = {i * j}\n";
+                }
+            }
+        }
+
+        private void BtnWissen_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResultaat.Clear();
+            TxtGetal.Clear();
+        }
+
+        private void BtnSluiten_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Toepassing 22 - Dobbelsteen/Toepassing 22 - Dobbelsteen/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_22___Dobbelsteen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Berekenen();
+        }
+
+        private void BtnOpnieuw_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResultaat.Clear();
+            BtnStart.Focus();
+        }
+
+        private void BtnSluiten_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Berekenen()
+        {
+            Random r = new Random();
+            int result = 0;
+            int counter = 1;
+            
+            while(result != 6)
+            {
+                result = r.Next(1, 7);
+                TxtResultaat.Text += $"Worp {counter} geeft {result}\n";
+                counter++;
+            }
+        }
+    }
+}
